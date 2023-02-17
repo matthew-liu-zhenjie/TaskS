58064da662464aa35af9c34a188a39e4c2a91ee5
Matthew Liu <matthew.liu.zhenjie@gmail.com>
2023-02-17 17:31:51 +0800

    Add functionality using lambdas

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -6,6 +6,9 @@ import se.edu.inclass.task.Task;
 import se.edu.inclass.task.TaskNameComparator;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
@@ -20,10 +23,23 @@ public class Main {
         System.out.println();
         System.out.println("Printing deadlines");
         printDeadlines(tasksData);
-
+        printDeadlinesInSortedOrder(tasksData);
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        ArrayList<Task> filteredList = filterTaskListUsingStreams(tasksData, "11");
+        System.out.println("\nTask filtered by String:");
+        printData(filteredList);
     }
+
+    public static ArrayList<Task> filterTaskListUsingStreams(ArrayList<Task> Tasks, String filterString) {
+        ArrayList<Task> filteredList;
+        filteredList = (ArrayList<Task>) Tasks.stream()
+                .filter(t -> t.getDescription().contains(filterString))
+                .collect(toList());
+
+        return filteredList;
+    }
+
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
         int count = 0;
@@ -40,7 +56,13 @@ public class Main {
             System.out.println(t);
         }
     }
-
+    public static void printDeadlinesInSortedOrder(ArrayList<Task> Tasks){
+        System.out.println("\nDeadlines in sorted order");
+        Tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().compareToIgnoreCase(b.getDescription()))
+                .forEach(System.out::println);
+    }
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
